8b9c21cedea7480d55770166cba4c35774c7aa24
Rodrigo Hernandez <172551303+rodrigohernandez1@users.noreply.github.com>
2024-11-30 21:09:21 -0600

    make dictionary from both lists

--- user_info.py
+++ user_info.py
@@ -12,7 +12,7 @@ risk_opinion = int(input("del uno al 10 cálifique su tolerancía al riesgo, 1 q
 #checar fallas de entrada del 1 al 10 
 
 
-#cargar el archivo de grupos de rendimientos de las 10 diferentes SIENFORES, checar apéndice 1
+#cargar el archivo de grupos de rendimientos promedio en los últimos 5 años de las 10 diferentes SIENFORES del Gobierno de México, checar apéndice 1
 with open('src/SIENFORE_returns.txt', 'r', encoding = 'utf-8-sig') as file: #el encoding le quita el "ufeff" de formato que le pone el editor de texto
     afore_file = file.read()
 afore_file = afore_file.split('\n')
@@ -24,8 +24,8 @@ for group in afore_file:
     returns = float(group[-4:])
     afore_returns.append(returns) #separando los retornos del file de texto original 
     afore_groups.append(group[0: -6]) #separando el nombre de los distintos grupos del afore, los SIENFOREs
-print(afore_returns)
-print(afore_groups)
+afore_dict = dict(zip(afore_groups, afore_returns))
+print(afore_dict['Básica Inicial'])
 
 
 
