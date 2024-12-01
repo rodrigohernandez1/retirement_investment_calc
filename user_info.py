5cd5475b1d9e6c1610765fdb48ed4a347e3207bc
Rodrigo Hernandez <172551303+rodrigohernandez1@users.noreply.github.com>
2024-11-30 18:55:55 -0600

    grammar correction

--- user_info.py
+++ user_info.py
@@ -4,7 +4,7 @@
 #preguntar por los datos del usuario 
 name = input("ingrese su nombre")
 age = int(input("ingrese su edad como número entero"))
-#checar fayas de entrada de edad
+#checar fallas de entrada de edad
 if age < 18 or age >= 65 or type(age) != int:
     age = int(input("ingrese una edad válida del 18 al 64"))
 risk_opinion = int(input("del uno al 10 cálifique su tolerancía al riesgo, 1 que quiere tomar el MENOR riesgo posible y 10 es que le encanta el riesgo"))
