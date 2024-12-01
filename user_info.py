4dc2582272f00a4a8f4e450703359eaddda024d9
Rodrigo Hernandez <172551303+rodrigohernandez1@users.noreply.github.com>
2024-11-30 19:27:31 -0600

    open SIENFORE txt file

--- user_info.py
+++ user_info.py
@@ -5,9 +5,17 @@
 name = input("ingrese su nombre")
 age = int(input("ingrese su edad como número entero"))
 #checar fallas de entrada de edad, 
-if age < 16 or age >= 65: #la edad es de mínima de 16 ya que es la edad mínima para esta en IMSS en México y 64 es la edad máxima para ahorrar antes de los 65
+if age < 16 or age >= 120: #la edad es de mínima de 16 ya que es la edad mínima para esta en IMSS en México y 120 es más que el récord de edad más vieja de México
     age = int(input("ingrese una edad válida del 18 al 64"))
 risk_opinion = int(input("del uno al 10 cálifique su tolerancía al riesgo, 1 que quiere tomar el MENOR riesgo posible y 10 es que le encanta el riesgo"))
+#checar fallas de entrada del 1 al 10 
+
+
+#cargar el archivo de grupos de edad de las 10 diferentes SIENFORES, checar apéndice 1
+with open('src/SIENFORE_groups.txt', 'r') as file:
+    afore_groups = file.read() 
+    print(afore_groups)
+
 
 
 
