28093844374255b3f7db243cc338b317524c27f0
Rodrigo Hernandez <172551303+rodrigohernandez1@users.noreply.github.com>
2024-12-03 12:53:42 -0600

    period_change function

--- user_info.py
+++ user_info.py
@@ -98,7 +98,31 @@ def afore_default_contribution(salary):
     government_contribution = bimestral_salary * 0.00225 #la contribución del goberino sobre tu salario
     total_contribution = worker_contribution + employer_contribution + government_contribution
     return total_contribution
+#%%
+def interest_period_change(annual_return, months):
+    """
+    Esta función cambia el interés anual a interés equivalente compuesto en un periodo diferente al anual.  
 
+    Parameters
+    ----------
+    annual_return :  float 
+        el retorno anual que quiere ser convertido a otro periodo, expresado como porcentaje (5.7% se escribe sin el signo: 5.7) 
+    months: int
+        los meses al cual el interés será convertido. 
+
+    Returns
+    -------
+    period_rate convertido compuesto 
+
+    """ 
+    base = 100
+    final = 100 * (1 + (annual_return/100))
+    period_fraction = 12 / months #la fracción del periodo con relación a un año, si el periodo son 2 meses, la fracción es 1/6 ya que hay 6 bimestres en el año
+    period_rate = (final/base)**(1/period_fraction) - 1
+    period_rate_percentage = period_rate * 100
+    return period_rate
+
+print(interest_period_change(12, 2))
 
 
  
@@ -134,3 +158,5 @@ def afore_default_contribution(salary):
 
 
 
+
+# %%
