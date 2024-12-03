8c3a4e665f2b15637aa5bc4c873ebf1d23681b33
Rodrigo Hernandez <172551303+rodrigohernandez1@users.noreply.github.com>
2024-12-03 17:20:21 -0600

    contribution function move to afore module

--- afore_module_functions.py
+++ afore_module_functions.py
@@ -20,5 +20,25 @@ def afore_assignment(age, afore_dict):
         age_range = afore_dict[group]
         if age <= age_range[1] and age >= age_range[0]: #busca si la edad está entre el rango de edad de la tupla
             return group
+def afore_default_contribution(salary): 
+    """
+    Esta función calcula la contribución total al AFORE bimestralmente basada en el salario sin contar las contribuciones voluntarias 
+
+    Parameters
+    ----------
+    salary :  float 
+        el salario del usuario 
+
+    Returns
+    -------
+    contribución total al AFORE sin contribuciones voluntarias, considerando el trabajdor está dado de alta en el IMSS 
+
+    """ 
+    bimestral_salary = salary * 2 #el salario bimestral 
+    worker_contribution = bimestral_salary * 0.01125 #la contribución obligatoria del trabajdor es de 1.125% del salario, y es bimestral 
+    employer_contribution = bimestral_salary * 0.0515 #contribución del empleador es de 5.15% del salario, vease apéndice 4 
+    government_contribution = bimestral_salary * 0.00225 #la contribución del goberino sobre tu salario
+    total_contribution = worker_contribution + employer_contribution + government_contribution
+    return total_contribution
 
         
